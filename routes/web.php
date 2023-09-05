241b8eb34d1cae28138e20986fb8b2a266ed8913
Luega <martinelliluca90@gmail.com>
2023-09-05 15:18:54 +0200

    added auth

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() { 
-    return redirect('/photos');
+Auth::routes();
+
+Route::get('/', function () {
+    return view('home');
 });
 
-Route::resource('/photos', PhotoController::class);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Il prefisso delle rotte é admin
+// Il namespace é admin 
+// Il middleware vede se sei autenticato o no
+Route::prefix('admin')->middleware('auth')->group(function () {
+
+    Route::resource('photos', PhotoController::class);
+   
+});
