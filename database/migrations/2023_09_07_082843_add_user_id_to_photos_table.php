7e5358af3849d68346686ec5f3fdef18b4523a36
Luega <martinelliluca90@gmail.com>
2023-09-07 10:43:29 +0200

    added new migration to add user_id column to photos table

--- database/migrations/2023_09_07_082843_add_user_id_to_photos_table.php
+++ database/migrations/2023_09_07_082843_add_user_id_to_photos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTitleToPhotos extends Migration
+class AddUserIdToPhotosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddTitleToPhotos extends Migration
     public function up()
     {
         Schema::table('photos', function (Blueprint $table) {
-            $table->string('title', 100)->after('id');
+            $table->foreignId('user_id')->cascadeOnDelete();
         });
     }
 
@@ -26,7 +26,7 @@ class AddTitleToPhotos extends Migration
     public function down()
     {
         Schema::table('photos', function (Blueprint $table) {
-            $table->dropColumn('title');
+            $table->dropColumn('photos_user_id_foreign');
         });
     }
 }
